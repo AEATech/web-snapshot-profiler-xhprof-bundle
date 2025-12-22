29b9741bcc8785c423537a29b3157a5c804a7310
Eugene Kosogin <ekosogin@gmail.com>
2025-12-22 17:57:23 +0200

    Minor adjustments

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 use Symfony\Component\ErrorHandler\ErrorHandler;
 
-define('ROOT_PATH', realpath(__DIR__ . '/../'));
+define('ROOT_PATH', dirname(__DIR__));
 
 require_once ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
